9b392ebb07120005d0c8f9177e074aeb1e33b64a
Leobardo Cortes Manica <lcortes@dalai.com>
2018-07-15 20:39:14 -0500

    Added tests for URL model validations

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::UrlsController < ApplicationController
+
+  def create
+    @url = Url.new(params[:data])
+    parse_url = Nokogiri::HTML(open(params[:data][:attributes][:url].to_s))
+    # puts parse_url.class   # => Nokogiri::HTML::Document
+    @url.save
+  end
+
 end
 
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save the url without url presence" do
+    url = Url.new
+    assert !url.save, "Not saved the URL without a url"
+  end
+
+  test "should save the url now with a url presence" do
+    link = Url.new
+    link.url = "http://www.dalai.com/"
+    assert link.save, "Saved it with an URL"
+  end
 end
